35f1eeb40669a7aab47947e722e64d43d9de009a
Scott Ricketts <SJR@imac.lan>
2020-08-31 21:50:01 -0500

    Add time.sleep. App.py now working.

--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -20,6 +20,7 @@ def scrape_info():
     # Mars News
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(url)
+    time.sleep(2)
 
     # Create BeautifulSoup object
     html=browser.html
